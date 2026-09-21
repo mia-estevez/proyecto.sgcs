107cca09efad46dc62d05d6539182c171b83a8e2
mia-estevez <miaolivera88@gmail.com>
2026-09-20 23:48:28 -0300

    Creando página de calidad con html, css y js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@ document.addEventListener("click", function (evento) {
         menuDesplegable.style.display = "none";
     }
 });
+
+// =========================================
+// ACORDEÓN DE ISO 9126 / ISO 25000
+// =========================================
+
+const acordeones = document.querySelectorAll(".acordeon-calidad");
+
+acordeones.forEach(function (acordeon) {
+
+    const boton = acordeon.querySelector(".acordeon-titulo");
+
+    boton.addEventListener("click", function () {
+
+        // Verificamos si esta tarjeta ya está abierta
+        const estabaAbierto = acordeon.classList.contains("activo");
+
+        // Cerramos todas las tarjetas
+        acordeones.forEach(function (otroAcordeon) {
+            otroAcordeon.classList.remove("activo");
+        });
+
+        // Si estaba cerrada, la abrimos
+        if (!estabaAbierto) {
+            acordeon.classList.add("activo");
+        }
+
+    });
+
+});
